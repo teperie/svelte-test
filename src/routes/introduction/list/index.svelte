95aae041e6d2cab2194e1edd0f86029bea4e9fb8
teperie <77098015+teperie@users.noreply.github.com>
2023-05-21 18:15:20 +0900

    init

---
+++ src/routes/introduction/list/index.svelte
@@ -0,0 +1,10 @@
+<script>
+    let items = [{name: 'a'}, {name: 'b', desc: 'lol'}, {name: 'c', desc:'hey'}]
+</script>
+
+<h1>list</h1>
+<ul>
+	{#each items as item}
+		<a href={`./list/${item.name}`}><li>{item.name} x {item.desc}</li></a>
+	{/each}
+</ul>
